6b915890c0bd9f3d3fc742624abfe28c3ed99da8
Gabriel Alexandre  Cardoso <114122280+gabrielpoukas@users.noreply.github.com>
2026-02-13 20:56:56 -0300

    update

---
+++ script.js
@@ -0,0 +1,26 @@
+const dataSpan = document.getElementById("dataAtual");
+dataSpan.textContent = new Date().toLocaleDateString("pt-BR");
+
+const sections = document.querySelectorAll(".section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop &&
+        window.scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
